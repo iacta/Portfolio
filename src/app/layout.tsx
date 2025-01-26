538579e220a4f1b3f56d415dfe56f365f14cc13f
iacta <thiagobahiavitor@gmail.com>
2025-01-26 13:08:22 -0300

    bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,26 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Montserrat } from "next/font/google";
-import { openGraphImage } from '../../public/icon.png';
 
 const montserrat = Montserrat({
   variable: "--font-montserrat-sans",
   subsets: ["latin"],
 });
 
+import openGraphImage from '../../public/icon.png';
+
 export const metadata: Metadata = {
   title: "Portfólio",
-  description: "Um simples portfólio",
+  description:"Bem-vindo ao meu portfólio! Aqui, você encontrará uma coleção de projetos e soluções criadas com dedicação e expertise. Como desenvolvedor Fullstack, uso tecnologias modernas para construir experiências digitais únicas e eficientes. Navegue pelo meu trabalho e descubra como posso transformar suas ideias em realidade com soluções web inovadoras.",
   openGraph: {
-    ...openGraphImage,
+    images: [
+      {
+        url: openGraphImage.src,  
+        width: 800,
+        height: 600,
+        alt: 'Portfólio',
+      },
+    ],
     title: 'Portfólio',
   },
 };
